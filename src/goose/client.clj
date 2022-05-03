1caf8685d5c1b7c3d6d1fd510161379e5423a0c5
akshat <olttwa@users.noreply.github.com>
2022-05-03 16:51:53 +0530

    Address PR comments

--- src/goose/client.clj
+++ src/goose/client.clj
@@ -1,8 +1,30 @@
 (ns goose.client
   (:require
-    [goose.validations.async :as v]
     [goose.redis :as r]
-    [taoensso.carmine :as car]))
+    [taoensso.carmine :as car]
+    [clojure.edn :as edn]))
+
+(defn- qualified-fn-symbol?
+  "A function must be a qualified symbol."
+  [s]
+  (and
+    (qualified-symbol? s)
+    (resolve s)))
+
+(defn- edn-serializable-args?
+  "Returns true if args are edn-serializable.
+  BUG-FIX/Edge-cases:
+  - edn is serializing symbolized functions to a list.
+  - TODO: Modify args validation to have an exhaustive list.
+  - Refer Sidekiq best practices on job params."
+  [args]
+  (= args
+     (edn/read-string (str args))))
+
+(defn- non-negative-retries?
+  "Returns true if num is non-negative."
+  [num]
+  (not (neg? num)))
 
 (def default-queue "goose/queue:default")
 
@@ -16,8 +38,10 @@
   - Args must be edn-serializable
   - Retries must be non-negative
   edn: https://github.com/edn-format/edn"
-  ([qualified-fn-symbol & {:keys [args retries]
-                           :or   {args    nil
-                                  retries 0}}]
-   {:pre [(v/async qualified-fn-symbol args retries)]}
-   (r/wcar* (car/rpush default-queue [qualified-fn-symbol args]))))
+  [qualified-fn-symbol & {:keys [args retries]
+                          :or   {args    nil
+                                 retries 0}}]
+  {:pre [(qualified-fn-symbol? qualified-fn-symbol)
+         (edn-serializable-args? args)
+         (non-negative-retries? retries)]}
+  (r/wcar* (car/rpush default-queue [qualified-fn-symbol args])))
