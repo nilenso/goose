f53e1278a43215a2bd586a58f0210d6fcc24dcb0
akshat <olttwa@users.noreply.github.com>
2022-08-29 20:11:39 +0530

    [rmq] Test scheduled jobs

--- test/goose/rmq/integration_test.clj
+++ test/goose/rmq/integration_test.clj
@@ -24,3 +24,29 @@
       (is (uuid? (UUID/fromString (c/perform-async tu/rmq-client-opts `perform-async-fn arg))))
       (is (= arg (deref perform-async-fn-executed 100 :e2e-test-timed-out)))
       (w/stop worker))))
+
+; ======= TEST: Relative Scheduling ==========
+(def perform-in-sec-fn-executed (promise))
+(defn perform-in-sec-fn [arg]
+  (deliver perform-in-sec-fn-executed arg))
+
+(deftest perform-in-sec-test
+  (testing "[rmq] Goose executes a function scheduled in future"
+    (let [arg "scheduling-test"
+          _ (c/perform-in-sec tu/rmq-client-opts 1 `perform-in-sec-fn arg)
+          worker (w/start tu/rmq-worker-opts)]
+      (is (= arg (deref perform-in-sec-fn-executed 1100 :scheduler-test-timed-out)))
+      (w/stop worker))))
+
+; ======= TEST: Absolute Scheduling (in-past) ==========
+(def perform-at-fn-executed (promise))
+(defn perform-at-fn [arg]
+  (deliver perform-at-fn-executed arg))
+
+(deftest perform-at-test
+  (testing "[rmq] Goose executes a function scheduled in past"
+    (let [arg "scheduling-test"
+          _ (c/perform-at tu/rmq-client-opts (java.time.Instant/now) `perform-at-fn arg)
+          scheduler (w/start tu/rmq-worker-opts)]
+      (is (= arg (deref perform-at-fn-executed 100 :scheduler-test-timed-out)))
+      (w/stop scheduler))))
