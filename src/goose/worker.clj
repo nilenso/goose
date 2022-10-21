968923123d50c21da70a6702cd4eed994bac3574
akshat <olttwa@users.noreply.github.com>
2022-10-21 10:42:47 +0530

    Fix `No implementation of protocol method` bug
    - Reloading a REPL nullifies all existing defrecord implementations
    - `:extend-via-metadata` helps circumvent this bug
    - https://clojurians.slack.com/archives/C03S1KBA2/p1666247372091009
    - https://clojure.org/reference/protocols#_extend_via_metadata

--- src/goose/worker.clj
+++ src/goose/worker.clj
@@ -5,7 +5,10 @@
     [goose.metrics.statsd :as statsd]))
 
 (defprotocol Shutdown
+  ;; We're extending a protocol via metadata because reloading REPL
+  ;; nullifies all existing defrecord implementations.
   "Shutdown a worker object."
+  :extend-via-metadata true
   (stop [this]))
 
 (def default-opts
