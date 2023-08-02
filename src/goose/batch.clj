dfc0e770dafd37bdb72ece8aa51ab1295ae35280
Siri P R <siri.pr914@gmail.com>
2023-08-02 19:42:28 +0530

    feat: batch status API
    chore: address PR review
    
    test: add timeout to invalid id test
    
    fix: redis conn error
    
    chore: address PR review
    
    chore: remove unnecessary call to perform-batch in api test
    
    style: alpahbetically order requires

--- src/goose/batch.clj
+++ src/goose/batch.clj
@@ -1,9 +1,22 @@
-(ns goose.batch)
+(ns goose.batch
+  (:require
+    [goose.utils :as u]))
+
+(def status-in-progress "in-progress")
+(def status-complete "complete")
 
 (defn new
   [{:keys [callback-fn-sym]} jobs]
   (let [id (str (random-uuid))]
     {:id              id
      :callback-fn-sym callback-fn-sym
-     :jobs            (map #(assoc % :batch-id id) jobs)}))
+     :jobs            (map #(assoc % :batch-id id) jobs)
+     :created-at      (u/epoch-time-ms)}))
+
+(defn status-from-counts
+  [{:keys [successful dead]}
+   total]
+  (cond
+    (= (+ successful dead) total) status-complete
+    :else status-in-progress))
 
--- src/goose/brokers/redis/batch.clj
+++ src/goose/brokers/redis/batch.clj
@@ -3,12 +3,13 @@
             [goose.defaults :as d]
             [goose.job :as job]
             [goose.retry]
+            [goose.utils :as u]
             [taoensso.carmine :as car]))
 
 (defn- set-batch-state
   [{:keys [id jobs] :as batch}]
   (let [batch-state-key (d/prefix-batch id)
-        batch-state (select-keys batch [:id :callback-fn-sym])
+        batch-state (select-keys batch [:id :callback-fn-sym :created-at])
         enqueued-job-set (d/construct-batch-job-set id d/enqueued-job-set)
         job-ids (map :id jobs)]
     (car/hmset* batch-state-key batch-state)
@@ -46,3 +47,26 @@
               (redis-cmds/move-between-sets redis-conn src dst id)
               (throw ex)))))
       (next opts job))))
+
+(defn get-batch-state
+  [redis-conn id]
+  (let [batch-state-key (d/prefix-batch id)
+        enqueued-job-set (d/construct-batch-job-set id d/enqueued-job-set)
+        retrying-job-set (d/construct-batch-job-set id d/retrying-job-set)
+        successful-job-set (d/construct-batch-job-set id d/successful-job-set)
+        dead-job-set (d/construct-batch-job-set id d/dead-job-set)
+        [_ [batch-state enqueued retrying successful dead]]
+        (car/atomic redis-conn
+                    redis-cmds/atomic-lock-attempts
+                    (car/multi)
+                    (car/hgetall batch-state-key)
+                    (car/scard enqueued-job-set)
+                    (car/scard retrying-job-set)
+                    (car/scard successful-job-set)
+                    (car/scard dead-job-set))]
+    (when (not-empty batch-state)
+      {:batch-state (u/flat-sequence->map batch-state)
+       :enqueued    enqueued
+       :retrying    retrying
+       :successful  successful
+       :dead        dead})))
