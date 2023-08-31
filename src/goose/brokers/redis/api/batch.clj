bad12cb8bf3581dbfc4c93eb6861093995607520
akshat <olttwa@users.noreply.github.com>
2023-08-31 19:11:37 +0530

    [redis] Test `batch/delete` API

--- src/goose/brokers/redis/api/batch.clj
+++ src/goose/brokers/redis/api/batch.clj
@@ -8,30 +8,36 @@
 (defn status [redis-conn id]
   (batch/get-batch-state redis-conn id))
 
+(defn- delete-enqueued-jobs
+  [redis-conn
+   {:keys [queue]}
+   enqueued-job-set]
+  (let [enqueued-job-ids (redis-cmds/set-members redis-conn enqueued-job-set)]
+    (doseq [job-id enqueued-job-ids]
+      (when-let [job (enqueued-jobs/find-by-id redis-conn queue job-id)]
+        (enqueued-jobs/delete redis-conn job)))))
+
+(defn- delete-retrying-jobs
+  [redis-conn
+   {{:keys [retry-queue ready-retry-queue]} :retry-opts :keys [queue ready-queue]}
+   retrying-job-set]
+  (let [retried-job-ids (redis-cmds/set-members redis-conn retrying-job-set)]
+    (doseq [job-id retried-job-ids]
+      (if-let [job-scheduled-for-retry (scheduled-jobs/find-by-id redis-conn job-id)]
+        (scheduled-jobs/delete redis-conn job-scheduled-for-retry)
+        (let [queue (or retry-queue queue)
+              retry-or-ready-queue (or ready-retry-queue ready-queue)]
+          (when-let [job-enqueued-for-retry (enqueued-jobs/find-by-id redis-conn queue job-id)]
+            (enqueued-jobs/delete redis-conn job-enqueued-for-retry retry-or-ready-queue)))))))
+
 (defn delete [redis-conn id]
   (let [batch-state-key (d/prefix-batch id)
         enqueued-job-set (d/construct-batch-job-set id d/enqueued-job-set)
         retrying-job-set (d/construct-batch-job-set id d/retrying-job-set)
         successful-job-set (d/construct-batch-job-set id d/successful-job-set)
         dead-job-set (d/construct-batch-job-set id d/dead-job-set)
-        {:keys [batch-state]} (batch/get-batch-state redis-conn id)
-        {{:keys [retry-queue]} :retry-opts :keys [queue]} batch-state]
+        {:keys [batch-state]} (batch/get-batch-state redis-conn id)]
 
-    ;; delete enqueued jobs
-    (let [enqueued-job-ids (redis-cmds/set-members redis-conn enqueued-job-set)]
-      (doseq [job-id enqueued-job-ids]
-        (when-let [job (enqueued-jobs/find-by-id redis-conn queue job-id)]
-          (enqueued-jobs/delete redis-conn job))))
-
-    ;; delete retrying jobs
-    (let [retried-job-ids (redis-cmds/set-members redis-conn retrying-job-set)]
-      (doseq [job-id retried-job-ids]
-        (let [retrying-job-queue (or retry-queue queue)
-              job-scheduled-for-retry (scheduled-jobs/find-by-id redis-conn job-id)]
-          (if job-scheduled-for-retry
-            (scheduled-jobs/delete redis-conn job-scheduled-for-retry)
-            (when-let [job-enqueued-for-retry (enqueued-jobs/find-by-id redis-conn retrying-job-queue job-id)]
-              (enqueued-jobs/delete redis-conn job-enqueued-for-retry))))))
-
-    ;; delete batch
+    (delete-enqueued-jobs redis-conn batch-state enqueued-job-set)
+    (delete-retrying-jobs redis-conn batch-state retrying-job-set)
     (redis-cmds/del-keys redis-conn batch-state-key enqueued-job-set retrying-job-set successful-job-set dead-job-set)))
