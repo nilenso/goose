53c69bfe418d59a5af31d2ca6768d26b71b6629c
akshat <olttwa@users.noreply.github.com>
2023-09-02 02:33:39 +0530

    Flatten batch-state map because destructuring a nested-map was very cumbersome

--- src/goose/api/batch.clj
+++ src/goose/api/batch.clj
@@ -3,17 +3,11 @@
     [goose.broker :as b]
     [goose.client]))
 
-(defn- into-batch-map
-  [{:keys [batch-state] :as batch}]
-  (let [counts (select-keys batch [:enqueued :retrying :successful :dead])
-        status (select-keys batch-state [:id :created-at :status :total-jobs])]
-    (merge status counts)))
-
 (defn status
   [broker id]
   (let [batch (b/batch-status broker id)]
     (when batch
-      (into-batch-map batch))))
+      (select-keys batch [:id :status :enqueued :retrying :successful :dead :total :created-at]))))
 
 (defn delete
   [broker id]
