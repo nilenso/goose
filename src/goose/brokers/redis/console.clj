ba47100c6b7c07b136da02a539af473b350299ba
Alisha Mohanty <alishamohanty1416@gmail.com>
2024-01-22 20:15:28 +0530

    console: Build redis landing page using hiccup
    Remove route-prefix from uri so that wrap-resource can load static files
    from resources dir

--- src/goose/brokers/redis/console.clj
+++ src/goose/brokers/redis/console.clj
@@ -1,7 +1,38 @@
 (ns goose.brokers.redis.console
-  (:require [ring.util.response :as response]))
+  (:require [hiccup.page :refer [html5 include-css]]
+            [ring.util.response :as response]))
 
-(defn handler [_broker _req]
+(defn landing-page [stats-map]
+  (html5 [:head
+          [:meta {:charset "UTF-8"}]
+          [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
+          [:title "Goose Dashboard"]]
+         (include-css "css/style.css")
+         [:body
+          [:header
+           [:nav
+            [:div.nav-start
+             [:div#logo "AppName"]
+             [:div#menu
+              [:a {:href "/enqueued"} "Enqueued"]
+              [:a {:href "/scheduled"} "Scheduled"]
+              [:a {:href "/periodic"} "Periodic"]
+              [:a {:href "/batch"} "Batch"]
+              [:a {:href "/dead"} "Dead"]]]]]
+          [:main
+           [:section.statistics
+            (for [stat [{:id :enqueued :label "Enqueued"}
+                        {:id :scheduled :label "Scheduled"}
+                        {:id :periodic :label "Periodic"}
+                        {:id :dead :label "Dead"}]]
+              [:div.stat {:id (:id stat)}
+               [:span.number (str (get stats-map (:id stat)))]
+               [:span.label (:label stat)]])]]]))
+
+(defn handler [_broker {:keys [uri]}]
   (case uri
-    "/" (response/not-found "<div> Redis Homepage </div>")
+    "/" (response/response (landing-page {:enqueued  2
+                                          :scheduled 10
+                                          :periodic  12
+                                          :dead      1}))
     (response/not-found "<div> Not found </div>")))
--- src/goose/console.clj
+++ src/goose/console.clj
@@ -1,6 +1,19 @@
 (ns goose.console
   (:require [goose.broker :as b]
-            [ring.middleware.resource :refer :all]))
+            [clojure.string :as string]
+            [ring.middleware.resource :refer :all]
+            [ring.middleware.resource :refer [wrap-resource]]))
+
+(defn wrap-remove-route-prefix [handler]
+  (fn [{:keys [route-prefix] :as req}]
+    (handler (update req :uri #(-> %1
+                                   re-pattern
+                                   (string/replace route-prefix ""))))))
 
 (defn handler [{:keys [broker] :as req}]
   (b/handler broker req))
+
+(def app-handler
+  (-> handler
+      (wrap-resource "public")
+      wrap-remove-route-prefix))
