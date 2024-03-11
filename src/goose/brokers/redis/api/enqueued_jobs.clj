ec657d370862692bfbf69cab4391748392926b19
Alisha Mohanty <alishamohanty1416@gmail.com>
2024-03-11 14:42:36 +0530

    [redis] Add doc strings to redis API

--- src/goose/brokers/redis/api/enqueued_jobs.clj
+++ src/goose/brokers/redis/api/enqueued_jobs.clj
@@ -4,26 +4,34 @@
     [goose.defaults :as d]
     [goose.job :as job]))
 
-(defn list-all-queues [redis-conn]
+(defn list-all-queues
+  "Lists all the queues"
+  [redis-conn]
   (map d/affix-queue (redis-cmds/find-lists redis-conn (str d/queue-prefix "*"))))
 
-(defn size [redis-conn queue]
+(defn size
+  "Returns count of jobs in the queue"
+  [redis-conn queue]
   (redis-cmds/list-size redis-conn (d/prefix-queue queue)))
 
-(defn find-by-pattern [redis-conn queue match? limit]
+(defn find-by-pattern
+  "Finds job/s by user-defined parameters in given queue within the given limit"
+  [redis-conn queue match? limit]
   (redis-cmds/find-in-list redis-conn (d/prefix-queue queue) match? limit))
 
-(defn find-by-id [redis-conn queue id]
+(defn find-by-id
+  "Finds a job by `:id` in given queue"
+  [redis-conn queue id]
   (let [limit 1
         match? (fn [job] (= (:id job) id))]
     (first (find-by-pattern redis-conn queue match? limit))))
 
 (defn prioritise-execution
+  "Moves job/s to front of the queue after verification of existence"
   ([redis-conn {:keys [ready-queue] :as job}]
    (when (redis-cmds/list-position redis-conn ready-queue job)
      (redis-cmds/del-from-list-and-enqueue-front redis-conn ready-queue job)))
 
-  ;; This isn't exposed by Broker API and is used internally by console
   ([redis-conn queue jobs]
    (let [remove-jobs-with-invalid-positions-fn (fn [p j] (->> (mapv vector p j)
                                                               (remove #(nil? (first %)))
@@ -33,18 +41,21 @@
      (redis-cmds/del-from-list-and-enqueue-front-multiple redis-conn (d/prefix-queue queue) jobs))))
 
 (defn delete
+  "Delete job/s from its queue"
   ([redis-conn job]
    (let [queue (job/ready-or-retry-queue job)]
      (= 1 (redis-cmds/del-from-list redis-conn queue job))))
 
-  ;; Used internally by console
   ([redis-conn queue jobs]
    (not-any? #{0} (redis-cmds/del-from-list-multiple redis-conn (d/prefix-queue queue) jobs))))
 
-(defn purge [redis-conn queue]
+(defn purge
+  "Purges all the jobs in the queue"
+  [redis-conn queue]
   (let [ready-queue (d/prefix-queue queue)]
     (= 1 (redis-cmds/del-keys redis-conn ready-queue))))
 
-;;Used internally by console
-(defn get-by-range [redis-conn queue start stop]
+(defn get-by-range
+  "Get all the jobs from start of queue to stop (inclusive)"
+  [redis-conn queue start stop]
   (redis-cmds/range-from-front redis-conn (d/prefix-queue queue) start stop))
