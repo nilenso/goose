3e324df20de50833656face5f7e2558838a64149
Alisha Mohanty <alishamohanty1416@gmail.com>
2024-04-01 15:53:24 +0530

    console: deconstruct args within console/app-handler fn itself

--- src/goose/console.clj
+++ src/goose/console.clj
@@ -1,8 +1,5 @@
 (ns goose.console
   (:require [goose.broker :as b]))
 
-(defn- handler [{{:keys [broker]} :console-opts :as req}]
-  (b/handler broker req))
-
-(defn app-handler [console-opts req]
-  (handler (assoc req :console-opts console-opts)))
+(defn app-handler [{:keys [broker] :as console-opts} req]
+  (b/handler broker (assoc req :console-opts console-opts)))
