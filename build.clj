5ad17801ecef305deeb56984f15f02c4d27e0776
Alisha Mohanty <alishamohanty1416@gmail.com>
2024-06-12 16:54:36 +0530

    Add test to build path
    To use factories namespace to add jobs easily

--- build.clj
+++ build.clj
@@ -9,17 +9,17 @@
 
 ;; delay to defer side effects (artifact downloads)
 (def basis (delay (b/create-basis {:project "deps.edn"
-                                   :aliases [:console]})))
+                                   :aliases [:console :test]})))
 
 (defn clean []
       (b/delete {:path "target"}))
 
 (defn uber [_]
       (clean)
-      (b/copy-dir {:src-dirs   ["src" "resources/public" "console"]
+      (b/copy-dir {:src-dirs   ["src" "resources/public" "console" "test"]
                    :target-dir class-dir})
       (b/compile-clj {:basis @basis
-                      :src-dirs   ["src" "resources/public" "console"]
+                      :src-dirs   ["src" "resources/public" "console" "test"]
                       :ns-compile '[goose.console-client]
                       :class-dir  class-dir})
       (b/uber {:class-dir class-dir
