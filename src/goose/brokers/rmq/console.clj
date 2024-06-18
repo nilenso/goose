803b54f13c9cd149eaa713538b4d61727f8453ad
Alisha Mohanty <alishamohanty1416@gmail.com>
2024-06-18 12:12:28 +0530

    console: View rmq's dead job page

--- src/goose/brokers/rmq/console.clj
+++ src/goose/brokers/rmq/console.clj
@@ -7,7 +7,7 @@
             [goose.utils :as u]
             [ring.util.response :as response]))
 
-(def header (partial console/header []))
+(def header (partial console/header [{:route "/dead" :label "Dead" :job-type :dead}]))
 
 (defn- stats-bar [{:keys [prefix-route] :as page-data}]
   [:main
@@ -18,24 +18,43 @@
        [:a {:href (prefix-route route)}
         [:span.label label]]])]])
 
+;; Declare dead-jobs queue to prevent rmq throwing exception
 (defn- declare-dead-queue
   [rmq-producer]
   (let [ch (u/random-element (:channels rmq-producer))]
     (rmq-queue/declare ch (merge (:queue-type rmq-producer)
                                  {:queue d/prefixed-dead-queue}))))
 
-(defn homepage [{:keys                     [prefix-route uri]
+(defn homepage [{:keys                     [prefix-route]
                  {:keys [app-name broker]} :console-opts}]
   (let [view (console/layout header stats-bar)
-        _ (declare-dead-queue broker)                       ;; Declare dead-jobs queue to prevent rmq throwing exception
+        _ (declare-dead-queue broker)
         data {:dead (dead-jobs/size (u/random-element (:channels broker)))}]
-    (response/response (view "Home" (assoc data :uri uri
-                                                :app-name app-name
+    (response/response (view "Home" (assoc data :app-name app-name
                                                 :prefix-route prefix-route)))))
+
+(defn job-page [_]
+  [:div.rmq
+   [:h1 "Dead Job"]
+   [:div.right
+    [:input {:type "number" :min "1" :max "10000" :placeholder "No. of jobs"}]
+    [:button.btn.btn-lg "Replay"]
+    [:button.btn-danger.btn-lg "Pop"]
+    [:button.btn-danger.btn-lg "Purge"]]])
+
+(defn get-dead-job [{:keys                     [prefix-route]
+                     {:keys [app-name broker]} :console-opts}]
+  (let [view (console/layout header job-page)
+        _ (declare-dead-queue broker)]
+    (response/response (view "Dead" {:job-type     :dead
+                                     :app-name     app-name
+                                     :prefix-route prefix-route}))))
+
 
 (defn- routes [route-prefix]
   [route-prefix [["" console/redirect-to-home-page]
                  ["/" homepage]
+                 ["/dead" {"" get-dead-job}]
                  ["/css/style.css" console/load-css]
                  ["/img/goose-logo.png" console/load-img]
                  ["/js/index.js" console/load-js]
