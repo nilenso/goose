334a0c143d351a5257817e1031a5d0c85f303f6e
Alisha Mohanty <alishamohanty1416@gmail.com>
2024-07-08 19:32:45 +0530

    console: Add apis to prioritise, delete, view >1 scheduled-jobs

--- src/goose/brokers/redis/api/scheduled_jobs.clj
+++ src/goose/brokers/redis/api/scheduled_jobs.clj
@@ -20,8 +20,26 @@
     (when (every? (comp not nil?) (redis-cmds/sorted-set-scores redis-conn sorted-set job))
       (redis-cmds/sorted-set->ready-queue redis-conn sorted-set (list job) job/ready-or-retry-queue))))
 
-(defn delete [redis-conn job]
-  (= 1 (redis-cmds/del-from-sorted-set redis-conn d/prefixed-schedule-queue job)))
+;; Inorder to continue support for existing API for prioritise-execution
+;; above fn is not modified, instead new fn is introduced to be used by console
+;; which returns jobs that were successfully prioritised instead of redis-txn
+(defn prioritises-execution [redis-conn & jobs]
+  (let [sorted-set d/prefixed-schedule-queue
+        scores (apply redis-cmds/sorted-set-scores redis-conn sorted-set jobs)
+        jobs-with-valid-scores (->> (mapv vector jobs scores)
+                                    (remove #(nil? (second %)))
+                                    (mapv first))]
+    (when (> (count jobs-with-valid-scores) 0)
+      (redis-cmds/sorted-set->ready-queue redis-conn sorted-set jobs job/ready-or-retry-queue))
+    jobs-with-valid-scores))
+
+(defn delete [redis-conn & jobs]
+  (= (count jobs) (apply redis-cmds/del-from-sorted-set redis-conn d/prefixed-schedule-queue jobs)))
 
 (defn purge [redis-conn]
   (= 1 (redis-cmds/del-keys redis-conn d/prefixed-schedule-queue)))
+
+;;Get scheduled jobs in increase order of their schedule-run-at i.e score
+(defn get-by-range
+  [redis-conn start stop]
+  (redis-cmds/range-in-sorted-set redis-conn d/prefixed-schedule-queue start stop))
