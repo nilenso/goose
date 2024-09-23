69ad901b08d7001bea34d7e01336c71ea9d43ef0
akshat <olttwa@users.noreply.github.com>
2024-09-23 19:28:14 +0530

    Refer to Cron as 'Cron Jobs' universally

--- src/goose/metrics.clj
+++ src/goose/metrics.clj
@@ -28,7 +28,7 @@
   (format "enqueued_jobs.%s.count" (d/affix-queue queue)))
 (defonce ^:no-doc total-enqueued-jobs-count "total_enqueued_jobs.count")
 (defonce ^:no-doc schedule-jobs-count "scheduled_jobs.count")
-(defonce ^:no-doc cron-count "cron.count")
+(defonce ^:no-doc cron-jobs-count "cron_jobs.count")
 (defonce ^:no-doc dead-jobs-count "dead_jobs.count")
 (defonce ^:no-doc batches-count "batches.count")
 
