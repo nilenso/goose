db14f436f9f6295ab5f72ef69a3abb15da903762
Sujai Kumar Gupta <sujayscience1234@gmail.com>
2024-11-18 19:42:57 +0530

    change broker record to encode an job before enqueuing

--- src/goose/brokers/sqs/broker.clj
+++ src/goose/brokers/sqs/broker.clj
@@ -3,13 +3,14 @@
    [goose.broker :as b]
    [goose.brokers.sqs.worker :as sqs-worker]
    [goose.brokers.sqs.sqs-requests :as sqs-request]
-   [cognitect.aws.client.api :as aws]))
+   [cognitect.aws.client.api :as aws]
+   [goose.utils :as u]))
 
 (defprotocol Close
   "Closes connection to SQS Message Broker."
   (close [this]))
 
-(defrecord SQS [client queue-url opts]
+(defrecord SQS [client opts]
   b/Broker
   ;; SQS supports nothing more than:
   ;;   - Enqueue a job
@@ -17,31 +18,42 @@
   ;;   - Enqueued jobs API
   ;;   - Batch jobs (Will implement later)
 
-  ;; Enqueue a job for immediate execution
   (enqueue
-    [_ job]
-    (sqs-request/enqueue client queue-url job))
+    [this job]
+    (let [{:keys [client]} this
+          {:keys [queue-url]} opts]
 
-  ;; Start a worker process
+      (sqs-request/enqueue client queue-url (u/encode-to-str job))))
+
   (start-worker
-    [_ worker-opts]
-    (sqs-worker/start client queue-url worker-opts))
-
-  ;; Enqueued jobs API
-  (enqueued-jobs-size
-    [_ _]
-    (sqs-request/size client queue-url))
-
-  (enqueued-jobs-purge
-    [_ _]
-    (sqs-request/purge client queue-url))
+    [this worker-opts]
+    (let [{:keys [client]} this
+          {:keys [queue-url]} opts]
+      (sqs-worker/start client queue-url worker-opts)))
 
   Close
   (close [_]
     (println "SQS client does not require explicit closure.")))
 
 (def default-opts
-  {:region "ap-south-1"})
+  "Default Config is not allowed like redis or rmq
+   
+   ### Keys
+   `:region`: AWS Region
+   Example: \"ap-south-1\" , \"us-east-1\"
+   
+   `:queue-url`: The Queue URL provided by the AWS SQS
+   Example: \"https://sqs.us-east-1.amazonaws.com/123456789012/MyQueue\""
+  {:region nil
+   :queue-url nil})
+
+(defn- create-sqs
+  "Helper function that creates an SQS broker implementation.
+  It abstracts the common logic for both producer and consumer."
+
+  [opts]
+  (let [client (aws/client {:api :sqs :region (:region opts)})]
+    (->SQS client opts)))
 
 (defn new-producer
   "Creates an SQS broker implementation for producer (client).
@@ -54,9 +66,7 @@
   (new-producer {:region \"us-east-1\" :queue-url \"https://sqs.us-east-1.amazonaws.com/123456789012/MyQueue\"})
   ```"
   [opts]
-  (let [client    (aws/client {:api :sqs :region (:region opts)})
-        queue-url (:queue-url opts)]
-    (->SQS client queue-url opts)))
+  (create-sqs opts))
 
 (defn new-consumer
   "Creates an SQS broker implementation for worker (consumer).
@@ -69,6 +79,4 @@
   (new-consumer {:region \"us-east-1\" :queue-url \"https://sqs.us-east-1.amazonaws.com/123456789012/MyQueue\"})
   ```"
   [opts]
-  (let [client    (aws/client {:api :sqs :region (:region opts)})
-        queue-url (:queue-url opts)]
-    (->SQS client queue-url opts)))
+  (create-sqs opts))
