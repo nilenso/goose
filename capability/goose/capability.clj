6fcbe6dfd36140111f75085674112f8cd72a5e34
Raj <rajp152k@gmail.com>
2026-04-09 15:29:39 +0530

    feat: evaluate broker capabilities

--- capability/goose/capability.clj
+++ capability/goose/capability.clj
@@ -1,5 +1,67 @@
-(ns goose.capability)
+(ns goose.capability
+  (:require
+   [clojure.set :refer [intersection difference]]
+   [clojure.edn :as e])
+  (:import [java.lang.reflect Modifier]))
+
+(defn- broker-name [implementation]
+  (.getName implementation))
+
+(defn- extract-methods [class]
+  (mapv first
+        (remove #(Modifier/isAbstract (second %))
+                (mapv (juxt #(.getName %) #(.getModifiers %))
+                      (.getMethods class)))))
+
+(def capabilities (mapv #(.getName %)
+                        (.getMethods goose.broker.Broker)))
+
+(defn- evaluate-broker [implementation]
+  (let [all-methods (set (extract-methods implementation))
+        cap-set     (set capabilities)
+        implemented (intersection cap-set all-methods)
+        lacking     (difference cap-set all-methods)]
+    {:Broker (.getSimpleName implementation)
+     :implemented    (vec implemented)
+     :lacking        (vec lacking)
+     :coverage       (str (format "%.2f"  (* 100
+                                             (float (/ (count implemented)
+                                                       (count cap-set)))))
+                          " %")}))
 
 (defn gauge [opts]
   "evaluates capabilities of broker implementations"
-  (println "evaluates capabilities of broker implementations"))
+  (let [implementations [goose.brokers.redis.broker.Redis
+                         goose.brokers.rmq.broker.RabbitMQ]]
+    (try
+      (->> implementations
+           (mapv evaluate-broker)
+           (reduce conj [])
+           (pr-str)
+           (spit "capability/goose/capability.edn"))
+      (catch Exception e
+        (println "Error evaluating broker capabilities:" (.getMessage e))))))
+
+(comment
+  (type (first (.getMethods goose.brokers.redis.broker.Redis)))
+  ;; java.lang.reflect.Method
+  ;; https://docs.oracle.com/javase/8/docs/api/java/lang/reflect/Method.html
+  ;; this exposes a getModifiers method
+  ;; rely on the class modifier having the abstract enum : https://docs.oracle.com/javase/8/docs/api/java/lang/reflect/Modifier.html
+  ;; jump into this java.lang.reflect.Modifier
+  ;; this also https://docs.oracle.com/javase/8/docs/api/java/lang/Class.html
+  ;; (the Class Class)
+  (.getModifiers
+   (first (.getMethods goose.brokers.redis.broker.Redis)))
+
+  (extract-methods  goose.brokers.redis.broker.Redis)
+
+  (extract-methods goose.brokers.rmq.broker.RabbitMQ)
+
+  (evaluate-broker goose.brokers.redis.broker.Redis)
+
+  (evaluate-broker goose.brokers.rmq.broker.RabbitMQ)
+
+  (gauge [])
+
+  )
