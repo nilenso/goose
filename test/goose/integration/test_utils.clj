11214c69996b4503ee321614b1f7b485090019a2
Raj <rajp152k@gmail.com>
2026-04-16 18:05:04 +0530

    test: integration test environment setup helper

--- test/goose/integration/test_utils.clj
+++ test/goose/integration/test_utils.clj
@@ -2,28 +2,59 @@
   (:require
    [goose.test-utils :as tu]
    [goose.capability :as c]
-   [clojure.set :as s]))
+   [clojure.set :as s]
+   [clojure.test :refer [deftest use-fixtures testing is]]))
 
 ;; map of test-types and capabilities required
 (def test-reqs
   {:async-execution-test
    #{}})
 
-
 ;; TODO : direct index leveraging older boilerplate rn
-;; refactor potential unexplored 
-;; per broker utilities
+;; refactor potential unexplored yet
 
+;; per broker utilities
 (def broker-utils
-  {:Redis {:fixtures {:each [tu/redis-fixture]
+  {:redis {:fixtures {:each [tu/redis-fixture]
                       :once []}
            :client-opts tu/redis-client-opts
            :worker-opts tu/redis-worker-opts}
-   :RabbitMQ {:fixtures {:each [tu/rmq-fixture]
+   :rabbitmq {:fixtures {:each [tu/rmq-fixture]
                          :once []}
               :client-opts tu/rmq-client-opts
               :worker-opts tu/rmq-worker-opts}})
 
+(defmacro switch-ns [broker test-type]
+  `(ns ~(symbol  (str broker "-" test-type))
+     (:require [~(symbol (str  "goose.integration." test-type)) :refer :all])))
+
+(comment
+  (switch-ns "redis" "async-execution-test")
+  )
+
+(defmacro register-fixtures
+  "registers :each and :once fixtures in the current test namespace"
+  [broker]
+  (letfn [(fetch-fixtures  [type]
+            (->> broker
+                 (keyword)
+                 (get broker-utils)
+                 (:fixtures)
+                 (type)))]
+    `(do
+       ~@(for [type [:each :once]]
+           `(use-fixtures ~type ~@(fetch-fixtures type))))))
+
+(comment
+  (register-fixtures "redis"))
+
+(defmacro  setup-test-environment [broker test-type]
+  `(do (switch-ns ~broker ~test-type)
+       (register-fixtures ~broker)))
+
+(comment
+  (setup-test-environment "redis" "async-execution-test")
+  )
 
 (defn broker-testable?
   "predicate on whether the broker implementation
