dd7c2b7baa7172900c28e6b327b86bda4da9902e
Raj <rajp152k@gmail.com>
2026-04-16 18:55:49 +0530

    test: crude async execution integration test

---
+++ test/goose/integration/async_execution_test.clj
@@ -0,0 +1,30 @@
+(ns goose.integration.async-execution-test
+  (:require
+   [goose.integration.test-utils :as tu]
+   [goose.client :as c]
+   [goose.worker :as w]
+   [clojure.string :as s]))
+
+(defmacro gen-async-execution-test [broker]
+  `(do
+     (tu/setup-test-environment broker "async-execution-test")
+     (let [perform-async-fn-executed (atom (promise))
+           perform-async-fn (fn [arg]
+                              (deliver @perform-async-fn-executed arg))]
+       (deftest ~(symbol (str "perform-" broker "-async-execution-test"))
+         (testing (str "Goose [" broker "] executes a function asynchronously")
+           (let [~'arg ::async-execute-test
+                 _ (c/perform-async (->> broker
+                                         (keyword)
+                                         (get tu/broker-utils)
+                                         (:client-opts))
+                                    ~`perform-async-fn
+                                    ~'arg)
+                 worker (w/start (->> broker
+                                      (keyword)
+                                      (get tu/broker-utils)
+                                      (:worker-opts)))]
+             (is (= arg (deref @perform-async-fn-executed 100 :e2e-test-timed-out)))
+             (w/stop-worker)))))))  
+
+(tu/gen-test-suite gen-async-execution-test)
