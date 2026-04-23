a1ce892ee024d87fd1d1ac045d2e5c31c36adc48
Raj <rajp152k@gmail.com>
2026-04-23 18:32:03 +0530

    misc: prune comments

--- test/goose/capability.clj
+++ test/goose/capability.clj
@@ -18,8 +18,6 @@
   [goose.brokers.redis.broker.Redis
    goose.brokers.rmq.broker.RabbitMQ])
 
-;; granularity of capabilities being presented from a user perspective : refer comments in the protocol
-;; testing utitities can continue with the functionality guarantees
 (defn- evaluate-broker [implementation]
   (let [all-methods (set (extract-methods implementation))
         implemented (intersection potential all-methods)
@@ -50,25 +48,3 @@
           (first)
           (:implemented)
           (set)))))
-
-(comment
-  (type (first (.getMethods goose.brokers.redis.broker.Redis)))
-  ;; java.lang.reflect.Method
-  ;; https://docs.oracle.com/javase/8/docs/api/java/lang/reflect/Method.html
-  ;; this exposes a getModifiers method
-  ;; rely on the class modifier having the abstract enum : https://docs.oracle.com/javase/8/docs/api/java/lang/reflect/Modifier.html
-  ;; jump into this java.lang.reflect.Modifier
-  ;; this also https://docs.oracle.com/javase/8/docs/api/java/lang/Class.html
-  ;; (the Class Class)
-  (.getModifiers
-   (first (.getMethods goose.brokers.redis.broker.Redis)))
-
-  (extract-methods  goose.brokers.redis.broker.Redis)
-
-  (extract-methods goose.brokers.rmq.broker.RabbitMQ)
-
-  (evaluate-broker goose.brokers.redis.broker.Redis)
-
-  (evaluate-broker goose.brokers.rmq.broker.RabbitMQ)
-
-  (gauge))
