93bebec1c059a6ebc09dd5e3a04f910c743daed1
Raj <rajp152k@gmail.com>
2026-04-23 14:22:24 +0530

    test: get-opts utility

--- test/goose/integration/test_utils.clj
+++ test/goose/integration/test_utils.clj
@@ -3,7 +3,7 @@
    [goose.test-utils :as tu]
    [goose.capability :as c]
    [clojure.set :as s]
-   [clojure.test :refer [deftest use-fixtures testing is]]
+   [clojure.test :refer [deftest use-fixtures testing is report]]
    [goose.specs :as specs]
    [goose.brokers.rmq.queue :as rmq-queue]))
 
@@ -18,14 +18,17 @@
   {:redis {:fixtures {:pre [specs/instrument
                             tu/clear-redis]
                       :post [tu/clear-redis]}
-           :client-opts tu/redis-client-opts
-           :worker-opts tu/redis-worker-opts}
+           :opts {:client tu/redis-client-opts
+                  :worker tu/redis-worker-opts}}
    :rabbitmq {:fixtures {:pre [specs/instrument
                                tu/rmq-delete-test-queues
                                rmq-queue/clear-cache]
                          :post [tu/rmq-delete-test-queues]}
-              :client-opts tu/rmq-client-opts
-              :worker-opts tu/rmq-worker-opts}})
+              :opts {:client tu/rmq-client-opts
+                     :worker tu/rmq-worker-opts}}})
+
+(defn get-opts [broker opts-type]
+  (get-in broker-utils [broker :opts opts-type]))
 
 (defn broker-testable?
   "predicate on whether the broker implementation
@@ -34,15 +37,20 @@
   (s/subset? requirements 
              (c/fetch-capabilities broker)))
 
-(defmacro with-fixtures [broker & body]
+(defmacro with-fixtures [broker failure-reporter & body]
   (letfn [(fetch-fixtures [broker pos]
             (map list
                  (get-in broker-utils
                          [broker :fixtures pos])))]
     `(do 
        ~@(fetch-fixtures broker :pre)
-       ~@body
-       ~@(fetch-fixtures broker :post))))
+       (try
+         ~@body
+         (catch Throwable ex#
+           (~failure-reporter ex#)
+           (throw ex#))
+         (finally
+           ~@(fetch-fixtures broker :post))))))
 
 (comment
   (with-fixtures :redis
