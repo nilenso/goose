610df5002e30d94c8f5aaea90397df10c518be34
Raj <rajp152k@gmail.com>
2026-04-24 14:33:40 +0530

    test: refactor scheduling tests

--- test/goose/integration/scheduling_test.clj
+++ test/goose/integration/scheduling_test.clj
@@ -9,47 +9,51 @@
 
 (def requirements #{:schedule})
 
-(def perform-scheduled-fn-executed (atom nil))
+(def execd-log (atom {}))
 
-(defn perform-scheduled-fn [arg]
-  (reset! perform-scheduled-fn-executed arg))
+(defn perform-scheduled-fn [test-name executed-flag]
+  (swap! execd-log assoc test-name executed-flag))
 
 (deftest  absolute-scheduling-test
   (doseq [broker (keys tu/broker-utils)]
-    (alter-meta! #'absolute-scheduling-test assoc :name (str (symbol broker) "-absolute-scheduling-test"))
-    (reset! perform-scheduled-fn-executed nil)
-    (if (tu/broker-testable? broker requirements)
-      (tu/with-fixtures broker
-        (fn [ex] (report {:type :default
-                          :message (ex-message ex)}))
-        (testing (str "Absolute Scheduling" broker)
-          (let [_ (c/perform-at (tu/get-opts broker :client)
-                                (Instant/now)
-                                `perform-scheduled-fn
-                                ::absolute-scheduling-test)
-                scheduler (w/start (tu/get-opts broker :worker))]
-            (Thread/sleep 100)
-            (is (= ::absolute-scheduling-test @perform-scheduled-fn-executed))
-            (w/stop scheduler))))
-      (report {:type :default
-               :message (str "Absolute Scheduling" broker " is not testable")}))))
+    (let [test-name (str (symbol broker) "-absolute-scheduling-test")]
+      (alter-meta! #'absolute-scheduling-test assoc :name test-name)
+      (if (tu/broker-testable? broker requirements)
+        (tu/with-fixtures broker
+          (fn [ex] (report {:type :default
+                            :message (ex-message ex)}))
+          (testing (str "Absolute Scheduling" broker)
+            (let [test-flag (random-uuid)
+                  _ (c/perform-at (tu/get-opts broker :client)
+                                  (Instant/now)
+                                  `perform-scheduled-fn
+                                  test-name
+                                  test-flag)
+                  scheduler (w/start (tu/get-opts broker :worker))]
+              (Thread/sleep 100)
+              (is (= test-flag (get @execd-log test-name)))
+              (w/stop scheduler))))
+        (report {:type :default
+                 :message (str "Absolute Scheduling" broker " is not testable")})))))
 
 (deftest  relative-scheduling-test
   (doseq [broker (keys tu/broker-utils)]
-    (alter-meta! #'relative-scheduling-test assoc :name (str (symbol broker) "-relative-scheduling-test"))
-    (reset! perform-scheduled-fn-executed nil)
-    (if (tu/broker-testable? broker requirements)
-      (tu/with-fixtures broker
-        (fn [ex] (report {:type :default
-                          :message (ex-message ex)}))
-        (testing (str "Relative Scheduling" broker)
-          (let [_ (c/perform-in-sec (tu/get-opts broker :client)
-                                    1
-                                    `perform-scheduled-fn
-                                    ::relative-scheduling-test)
-                scheduler (w/start (tu/get-opts broker :worker))]
-            (Thread/sleep 2100)
-            (is (= ::relative-scheduling-test @perform-scheduled-fn-executed))
-            (w/stop scheduler))))
-      (report {:type :default
-               :message (str "Relative Scheduling" broker " is not testable")}))))
+    (let [test-name (str (symbol broker) "-relative-scheduling-test")]
+      (alter-meta! #'relative-scheduling-test assoc :name test-name)
+      (if (tu/broker-testable? broker requirements)
+        (tu/with-fixtures broker
+          (fn [ex] (report {:type :default
+                            :message (ex-message ex)}))
+          (testing (str "Relative Scheduling" broker)
+            (let [test-flag (random-uuid)
+                  _ (c/perform-in-sec (tu/get-opts broker :client)
+                                      1
+                                      `perform-scheduled-fn
+                                      test-name
+                                      test-flag)
+                  scheduler (w/start (tu/get-opts broker :worker))]
+              (Thread/sleep 2100)
+              (is (= test-flag (get @execd-log test-name)))
+              (w/stop scheduler))))
+        (report {:type :default
+                 :message (str "Relative Scheduling" broker " is not testable")})))))
