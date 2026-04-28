53f2fef6e3f9a3e48ccc08bd9015aca0b32b6fb0
Raj <rajp152k@gmail.com>
2026-04-28 16:15:14 +0530

    test: async execution test refactor : generic executable and deliveries

--- test/goose/integration/async_execution_test.clj
+++ test/goose/integration/async_execution_test.clj
@@ -1,31 +1,31 @@
 (ns goose.integration.async-execution-test
   (:require
-   [goose.integration.test-utils :as tu]
+   [goose.integration.test-utils :as tu :refer [setup-test-promise
+                                                executable
+                                                delivered-execution]]
    [goose.client :as c]
    [goose.worker :as w]
    [clojure.test :refer [deftest is testing report]]))
 
 (def requirements #{:enqueue})
 
-(def perform-async-fn-executed (atom nil))
-
-(defn perform-async-fn [arg]
-  (reset! perform-async-fn-executed arg))
-
 (deftest  async-execution-test
   (doseq [broker (keys tu/broker-utils)]
-    (reset! perform-async-fn-executed nil)
-    (if (tu/broker-testable? broker requirements)
-      (tu/with-fixtures broker
-        (fn [ex] (report {:type :default
-                          :message (ex-message ex)}))
-        (testing (str "Async Execution" broker)
-          (let [_ (c/perform-async (tu/get-opts broker :client)
-                                   `perform-async-fn
-                                   ::async-execution-test)
-                worker (w/start (tu/get-opts broker :worker))]
-            (Thread/sleep 100)
-            (is (= ::async-execution-test @perform-async-fn-executed))
-            (w/stop worker))))
-      (report {:type :default
-               :message (str "Async execution" broker " is not testable")}))))
+    (let [test-name (str (symbol broker) "-async-execution-test")]
+      (if (tu/broker-testable? broker requirements)
+        (do
+          (setup-test-promise test-name)
+          (tu/with-fixtures broker
+            (fn [ex] (report {:type :default
+                             :message (ex-message ex)}))
+            (testing (str "Async Execution" broker)
+              (let [_ (c/perform-async (tu/get-opts broker :client)
+                                       `executable
+                                       test-name
+                                       ::async-execution-test)
+                    worker (w/start (tu/get-opts broker :worker))]
+                (is (= ::async-execution-test
+                       (delivered-execution test-name)))
+                (w/stop worker)))))
+        (report {:type :default
+                 :message (str "Async execution" broker " is not testable")})))))
